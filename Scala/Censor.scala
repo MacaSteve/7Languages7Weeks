52149296109cb782eef47a26c48ab4ac7232fe2b
Steven McCormick <steven.mccormick@suncorp.com.au>
2013-03-04 12:59:16 +1000

    Updating censor to read from a file.

--- Scala/Censor.scala
+++ Scala/Censor.scala
@@ -1,15 +1,28 @@
+import scala.io.Source
+
 trait Censor
 {
-	val wordReplacements = Map("Shoot" -> "Pucky", "Darn" -> "Beans")
+	var wordReplacements = Map("Shoot" -> "Pucky", "Darn" -> "Beans")
 
 	def censorWords(words: String) : String = 
 		wordReplacements.foldLeft(words)((censoredWords, keyVal) => censoredWords.replaceAll(keyVal._1, keyVal._2))
+
+	def loadReplacements(filename: String) =
+	{
+		val file = Source.fromFile(filename)
+		wordReplacements =  file.getLines.foldLeft(wordReplacements)((replacements, line) => 
+		{
+			val words = line.split(" -> ")
+			replacements ++ Map(words(0) -> words(1))
+		})
+	}
 }
 
 class CensorSentence(sentence: String) extends Censor
 {
-	override def toString = censorWords2(sentence)
+	override def toString = censorWords(sentence)
 }
 
-val myCensorWords = new CensorSentence("Darn Shoot")
+val myCensorWords = new CensorSentence("Where the Hell did I put my Darn Balls!")
+myCensorWords.loadReplacements("CensorWords.txt")
 println(myCensorWords)
