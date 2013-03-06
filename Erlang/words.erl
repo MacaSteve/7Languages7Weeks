dda49a75eea62e0a6de03066ff669cd8d4017b47
Steven McCormick <steven.mccormick@suncorp.com.au>
2013-03-06 23:15:21 +1000

    Words still not working.

--- Erlang/words.erl
+++ Erlang/words.erl
@@ -1,5 +1,6 @@
 -module(words).
 -export([inAString/1]).
 
-inAString(One) -> 1;
-inAString(One, Two | Rest) -> inAString(Rest) + 1.
+inAString([]) -> 0;
+inAString(["\\s+" | Tail]) -> inAString(Tail) + 1;
+inAString([_ | Tail]) -> inAString(Tail).
