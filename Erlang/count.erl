1fd6b5da30eb52a1ec951599e86cf5f6849017b2
Steven McCormick <steve_mac@iinet.net.au>
2013-03-07 21:27:19 +1000

    Added recursive call that doesn't need second parameter.

--- Erlang/count.erl
+++ Erlang/count.erl
@@ -7,3 +7,8 @@ upToMax(N,Max) when N > Max -> "Finished";
 upToMax(N,Max) when N =< Max ->
 	io:format("~p~n", [N]),
 	upToMax(N+1,Max).
+
+upToRev(0) -> "Finished";
+upToRev(N) ->
+	upToRev(N-1),
+	io:format("~p~n", [N]).
