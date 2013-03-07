51bae4879148401c2557074e4e84c6297f490bb4
Steven McCormick <steve_mac@iinet.net.au>
2013-03-07 21:21:51 +1000

    Got the counting recursion working.

--- Erlang/count.erl
+++ Erlang/count.erl
@@ -1,16 +1,9 @@
 -module(count).
--export([upTo/1]).
--export([toWord/1]).
+-compile(export_all).
 
-upTo(1) -> 1;
-upTo(N) -> string:concat(upTo(N-1), N).
+upTo(Max) -> upToMax(1,Max).
 
-toWord(2) -> two;
-toWord(3) -> three;
-toWord(4) -> four;
-toWord(5) -> five;
-toWord(6) -> six;
-toWord(7) -> seven;
-toWord(8) -> eight;
-toWord(9) -> nine;
-toWord(10) -> ten.
+upToMax(N,Max) when N > Max -> "Finished";
+upToMax(N,Max) when N =< Max ->
+	io:format("~p~n", [N]),
+	upToMax(N+1,Max).
