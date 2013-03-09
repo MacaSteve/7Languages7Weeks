74fa18d8bbb7a67c7abec783345c03f603b326e2
Steven McCormick <steve_mac@iinet.net.au>
2013-03-09 12:17:24 +1000

    Trying to get some unit tests working.

--- Erlang/ticTacToe.erl
+++ Erlang/ticTacToe.erl
@@ -5,8 +5,7 @@
 -export([isLineWinning/1]).
 -export([isThereAWinner/1]).
 -export([allTheSameSymbol/1]).
-
-% {x,x,x,x,x,x,x,x,x}
+-export([runTestCases/0]).
 
 winner(Board) ->
 	{P1, P2, P3, P4, P5, P6, P7, P8, P9} = Board,
@@ -29,3 +28,13 @@ isLineWinning(Line) -> allCrosses(Line) or allNaughts(Line).
 
 isThereAWinner([Line]) -> isLineWinning(Line);
 isThereAWinner([Head|Tail]) -> isLineWinning(Head) or isThereAWinner(Tail).
+
+runTestCases() ->
+	io:format("~p~n", ["test"]).
+	%io:format("~p~n", [winner({x,x,x,e,e,e,e,e,e})]),
+	%io:format("~p~n", [winner({e,e,e,o,o,o,e,e,e})]),
+	%io:format("~p~n", [winner({e,e,e,e,e,e,x,x,x})]),	
+	%io:format("~p~n", [winner({x,e,e,e,x,e,e,e,x})]),	
+	%io:format("~p~n", [winner({e,e,x,e,x,e,x,e,e})]),	
+	%io:format("~p~n", [winner({x,x,e,e,e,e,e,e,e})]).	
+	
